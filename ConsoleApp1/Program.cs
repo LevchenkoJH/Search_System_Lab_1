35f151a40b86bc8b764eabbb2875316670fb0110
Levchenko Nikita <n.levchenko@everypixel.ru>
2023-02-15 18:41:55 +0500

    base version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+Console.ReadLine();
+
+
+
+
+
---
+++ ConsoleApp1/Search.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Term
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        string Name;
+        /// <summary>
+        /// 
+        /// </summary>
+        int Frequency;
+        /// <summary>
+        /// 
+        /// </summary>
+        List<Documents> Documents;
+    }
+
+    internal class Documents
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        int Id;
+        /// <summary>
+        /// 
+        /// </summary>
+        int Frequency;
+        /// <summary>
+        /// 
+        /// </summary>
+        List<int> Positions;
+    }
+}
