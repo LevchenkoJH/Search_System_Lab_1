cf1111e21704c9ef715fda00802b1fd4d9b5eba5
PchelinAndrej <andrej.pchelin@gmail.com>
2023-02-16 16:09:56 +0500

    Добавил файл с текстом и изменил структуру кода

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ConsoleApp1;
+﻿using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace search_engine
+{
+    internal struct Term
+    {
+        public string term;
+        public int frenq;
+    }
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory.ToString() + "text_documents\\";
 
 
 
+            using (StreamReader sr = File.OpenText(path + "text_1.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    char c = (char)sr.Read();
 
+                    Console.WriteLine(c);
 
+                }
+            }
 
-Console.WriteLine("Hello, World!");
+            Console.WriteLine(path);
 
-
-Console.ReadLine();
-
-
-
-
-
+        }
+    }
+}
