a08c0baa2e20dbc2b3227ceb81e37cfb523517b0
Levchenko Nikita <70745236+LevchenkoJH@users.noreply.github.com>
2023-02-17 02:18:19 +0500

    Добавлен FileReader

---
+++ ConsoleApp1/FileReader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SearchSystem
+{
+    /// <summary>
+    /// Класс для работы с txt-файлом
+    /// </summary>
+    public class FileReader
+    {
+        /// <summary>
+        /// Расположение файла
+        /// </summary>
+        private string FilePath;
+        /// <summary>
+        /// Поток для считывания строк
+        /// </summary>
+        private StreamReader stream;
+
+        public FileReader(string filePath)
+        {
+            FilePath = filePath;
+            StreamReader _stream = OpenFile(filePath);
+            if (_stream != null)
+                stream = _stream;
+        }
+
+        private StreamReader OpenFile(string filePath)
+        {
+            try
+            {
+                Console.WriteLine("Open -> " + filePath);
+                return new StreamReader(filePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при открытии файла: " + e.Message);
+                return null;
+            }
+            finally
+            {
+                Console.WriteLine("Открытие файла");
+            }
+        }
+
+        public string ReadLine()
+        {
+            try
+            {
+                return stream.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при чтении файла: " + e.Message);
+                return null;
+            }
+            finally
+            {
+                Console.WriteLine("Считывание строки");
+            }
+        }
+
+        public void FileClose()
+        {
+            try
+            {
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при закрытии файла: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Закрытие файла");
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using ConsoleApp1;
-
-
-
-
-
+using SearchSystem;
 
 Console.WriteLine("Hello, World!");
 
+FileReader TEST_FILE = new FileReader("C:\\Users\\Nikita\\Desktop\\andrey\\Search_System_Lab_1\\ConsoleApp1\\TERMS.txt");
+
+string TEST_STRING = TEST_FILE.ReadLine();
+Console.WriteLine(TEST_STRING);
+
+TEST_STRING = TEST_FILE.ReadLine();
+Console.WriteLine(TEST_STRING);
+
+TEST_STRING = TEST_FILE.ReadLine();
+Console.WriteLine(TEST_STRING);
+
+TEST_FILE.FileClose();
 
 Console.ReadLine();
-
-
-
-
-
