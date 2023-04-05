63a9afea5f6d2d81b8b6ec76e0337e77fee7d459
Levchenko Nikita <70745236+LevchenkoJH@users.noreply.github.com>
2023-04-05 05:39:27 +0500

    Добавил диаграмму и класс File

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using ConsoleApp1;
+using SearchSystem;
 
 
 
---
+++ ConsoleApp1/SearchSystemModels.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchSystem
+{
+    internal class Term
+    {
+        /// <summary>
+        /// Название термина
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Сколько раз встречался во всех документах
+        /// </summary>
+        public int Frequency { get; set; }
+        /// <summary>
+        /// Документы в которых встречался
+        /// </summary>
+        public List<Document> Documents { get; set; }
+    }
+
+    internal class Document
+    {
+        /// <summary>
+        /// Файл (документ) на который ссылаемся
+        /// </summary>
+        public int DocumentId { get; set; }
+        /// <summary>
+        /// Сколько раз данный термин встречается в этом документе
+        /// </summary>
+        public int Frequency { get; set; }
+        /// <summary>
+        /// Позиции слова в документе (key - строка, value - символ)
+        /// </summary>
+        public List<KeyValuePair<int, int>> Positions { get; set; }
+    }
+
+    internal class File
+    {
+        /// <summary>
+        /// Документ (файл)
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Название файла (документа)
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Количество терминов в файле (документе)
+        /// </summary>
+        public int Frequency { get; set; }
+    }
+}
